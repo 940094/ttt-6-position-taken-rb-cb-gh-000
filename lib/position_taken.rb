c1e0e5d401f8ce8dd41ebaeac0e622602d53df17
Justin Shaw <easyworkemail@gmail.com>
2019-04-27 14:13:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || index == nil  # space on the board is empty
+  if board[index] == " " || board[index] == "" || board[index] == nil  # space on the board is empty
     return false
   else
     return true
